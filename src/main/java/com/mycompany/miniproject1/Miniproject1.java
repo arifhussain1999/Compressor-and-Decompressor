d1a3b6e5c4300e3f37136570eb642c73eb9a67c0
Arif Hussain <arifh9821@gmail.com>
2023-07-11 00:13:59 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/miniproject1/Miniproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miniproject1;
+
+/**
+ *
+ * @author Arif Hussain
+ */
+public class Miniproject1 {
+
+    public static void main(String[] args)
+    {
+       
+    }
+}
